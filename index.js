ecf412066fd11b001b0c26a4fcb537cbef1e9231
Tsering Tsering <dtsering2@gmail.com>
2022-07-18 22:43:55 -0400

    commit tree 83a4e53af0e0a49c135d6264225bb1863635757e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+
+function saturdayFun (activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+};
+
+function mondayWork(defaultActivity = "go to the office"){
+    return `This Monday, I will ${defaultActivity}.`
+};
+
+
+function wrapAdjective(pattern = "*"){
+    let result =  function (adjective = "special"){
+        return `You are ${pattern}${adjective}${pattern}!`
+    }
+    return result
+};
+
+
